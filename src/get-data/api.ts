439b74fbc4dae2c7dc2385ac36b74ec4a7c8b52c
buiyenlinh <buiyenlinh1406@gmail.com>
2021-10-06 10:46:15 +0700

    add yup

--- src/get-data/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-// const APIAuth = axios.create({
-//     baseURL: "https://api-nienluan.sharenows.com/api/v1/product/"
-// })
-
-// axios.defaults.baseURL = "https://api-nienluan.sharenows.com/api/v1/auth/";
-// axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token');
-
-// export default APIAuth;
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+
+export default defineComponent({
+  setup() {
+    const username = ref()
+    const password = ref()
+
+    return {
+      username, password
+    }
+  },
+})
+</script>
+
+
+<template>
+  <el-form label-position="left" label-width="120px">
+    <el-form-item label="Tên đăng nhập:">
+      <el-input v-model="username"></el-input>
+    </el-form-item>
+    <el-form-item label="Mật khẩu:">
+      <el-input v-model="password"></el-input>
+    </el-form-item>
+  </el-form>
+</template>
